92968aaf2264206560284c9c410f496309c25a53
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-03-13 15:10:20 -0400

    finished lab

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
-	#your code here
+  puts "What is the spell that enacts levitation?"
+  foo = gets.chomp
+  case foo
+    when "Wingardium Leviosa"
+      puts "You passed the quiz!"
+    else
+      puts "yuo are wrøng!1!"
+  end
 end
-
-
