e3d35c39e5fbc1132c555e3582844c0d10b6d5da
Dan O'Dea <dansbands@hotmail.com>
2017-09-29 23:22:37 +0000

    Done

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,16 @@
 
 def levitation_quiz
-	#your code here
+  puts "What is the spell that enacts levitation?"
+
+  answer = gets.chomp
+
+  until answer == "Wingardium Leviosa"
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+  end
+
+    puts "You passed the quiz!"
+
 end
 
-
+#levitation_quiz
