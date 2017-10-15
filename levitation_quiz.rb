2c7d39e926312245819a588280b34c3503b893c7
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-10-15 22:26:07 +0000

    2 Ways of completion.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,16 @@
 
 def levitation_quiz
-	#your code here
+  puts 'What is the spell that enacts levitation?'
+  input = gets
+  puts 'You passed the quiz!' if input == "Wingardium Leviosa"
 end
 
 
+def levitation_quiz
+  loop do
+    puts "What is the spell that enacts levitation?"
+    u_input = gets.chomp
+    break if u_input == "Wingardium Leviosa"
+  end
+  puts "You passed the quiz!"
+end
