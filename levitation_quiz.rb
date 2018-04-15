0d008475edfe15f15a36f4ade7ef7923fc20cf51
github username <github email address>
2018-04-15 18:40:12 -0400

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
 
 def levitation_quiz
-	#your code here
+  answer = ""
+	until answer == "Wingardium Leviosa"
+    puts 'What is the spell that enacts levitation?'
+    answer = gets.chomp
+  end
+  puts 'You passed the quiz!'
 end
-
-
