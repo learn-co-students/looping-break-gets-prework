5faac79c6718b0aaa251d19440fb4b87ac6628b0
sparkbold-git <sparkboldstudio@gmail.com>
2018-08-21 12:49:23 -0500

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
 	#your code here
+  answer = []
+  until answer == "Wingardium Leviosa"
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+  end
+  puts "You passed the quiz!"
 end
-
-
