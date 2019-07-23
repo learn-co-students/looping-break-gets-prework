98e1ddef8f2241a761945825af5e67415205625e
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-23 20:44:35 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
+  answer = ""
 	#your code here
+	while answer != "Wingardium Leviosa" do
+	  puts "What is the spell that enacts levitation?" 
+	  answer = gets.chomp
+	end
+	puts "You passed the quiz!"
 end
 
 
